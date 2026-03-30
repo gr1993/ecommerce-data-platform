597ba2e99a30f5f812e5a72441290d8d9398e3d3
hppro2024\smcore2024 <klpark@smcore.co.kr>
2026-03-30 16:50:15 +0900

    fix(analytics): ksqlDB에서 order-created 토픽에 0번 파티션만 처리하는 문제 수정 (처리 스트림의 파티션 수를 똑같이 3으로 맞춤)

--- analytics-service/src/main/resources/db/ksqldb/02_order_item_transform.sql
+++ analytics-service/src/main/resources/db/ksqldb/02_order_item_transform.sql
@@ -5,20 +5,24 @@ SET 'auto.offset.reset' = 'earliest';
 -- 하나의 주문(N개 아이템)을 N개의 개별 매출 레코드로 변환하여 새로운 토픽으로 발행
 -- 이 결과물은 ClickHouse Kafka Engine이 직접 구독
 CREATE STREAM IF NOT EXISTS analytics_order_item 
-WITH (KAFKA_TOPIC='analytics-order-item', VALUE_FORMAT='JSON') AS
-    SELECT
-        orderId AS order_id,
-        orderNumber AS order_number,
-        userId AS user_id,
-        orderStatus AS status,
-        orderedAt AS ordered_at,
-        EXPLODE(orderItems)->orderItemId AS order_item_id,
-        EXPLODE(orderItems)->productId AS product_id,
-        EXPLODE(orderItems)->productName AS product_name,
-        EXPLODE(orderItems)->categoryId AS category_id,
-        EXPLODE(orderItems)->categoryName AS category_name,
-        EXPLODE(orderItems)->quantity AS quantity,
-        CAST(EXPLODE(orderItems)->unitPrice AS DOUBLE) AS unit_price,
-        CAST(EXPLODE(orderItems)->totalPrice AS DOUBLE) AS total_price
-    FROM order_created_raw
-    EMIT CHANGES;
+WITH (
+  KAFKA_TOPIC='analytics-order-item', 
+  VALUE_FORMAT='JSON',
+  PARTITIONS=3
+) AS
+SELECT
+  orderId AS order_id,
+  orderNumber AS order_number,
+  userId AS user_id,
+  orderStatus AS status,
+  orderedAt AS ordered_at,
+  EXPLODE(orderItems)->orderItemId AS order_item_id,
+  EXPLODE(orderItems)->productId AS product_id,
+  EXPLODE(orderItems)->productName AS product_name,
+  EXPLODE(orderItems)->categoryId AS category_id,
+  EXPLODE(orderItems)->categoryName AS category_name,
+  EXPLODE(orderItems)->quantity AS quantity,
+  CAST(EXPLODE(orderItems)->unitPrice AS DOUBLE) AS unit_price,
+  CAST(EXPLODE(orderItems)->totalPrice AS DOUBLE) AS total_price
+FROM order_created_raw
+EMIT CHANGES;
